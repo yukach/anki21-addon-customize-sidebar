496788fd0b1d0e6899ff5ba6e221113a907e916d
Yury Kazakevich <yukach@gmail.com>
2019-08-30 23:13:33 +0300

    add flags

--- customize_sidebar.py
+++ customize_sidebar.py
@@ -8,7 +8,7 @@ from aqt.qt import QIcon
 from anki.hooks import wrap
 from operator import itemgetter
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 config = mw.addonManager.getConfig(__name__)
 #print("config is", config)
@@ -26,10 +26,27 @@ def my_stdTree(self, root):
     if config['show_item_leech']:
         item = self.CallbackItem(root, _("Leech"), self._filterFunc("tag:leech"))
         item.setIcon(0, QIcon(":/icons/tag.svg"))
+    if config['show_tree_flags']:
+        root = self.CallbackItem(root, _("Flags"), None)
+        root.setExpanded(False)
+        root.setIcon(0, QIcon(":/icons/tag.svg"))
+        item = self.CallbackItem(root, _("Red"), self._filterFunc("flag:1"))
+        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item = self.CallbackItem(root, _("Orange"), self._filterFunc("flag:2"))
+        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item = self.CallbackItem(root, _("Green"), self._filterFunc("flag:3"))
+        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item = self.CallbackItem(root, _("Blue"), self._filterFunc("flag:4"))
+        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item = self.CallbackItem(root, _("No"), self._filterFunc("flag:0"))
+        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item = self.CallbackItem(root, _("Any"), self._filterFunc("-flag:0"))
+        item.setIcon(0, QIcon(":/icons/tag.svg"))
 
 if config['show_item_marked'] or \
    config['show_item_suspended'] or \
-   config['show_item_leech']:
+   config['show_item_leech'] or \
+   config['show_tree_flags']:
     Browser._stdTree = wrap(Browser._stdTree, my_stdTree)
 
 #########################################
