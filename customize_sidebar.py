e1fbd92fdb12966d6e60e07233d766c8258df070
Yury Kazakevich <yukach@gmail.com>
2020-01-15 13:18:40 +0300

    v2.0.0-rc.2

--- customize_sidebar.py
+++ customize_sidebar.py
@@ -7,18 +7,25 @@ from aqt.browser import Browser
 from aqt.qt import QIcon
 from anki import version
 from anki.hooks import wrap
+from anki.lang import _
 from operator import itemgetter
 
-__version__ = "2.0.0-rc.1"
+__version__ = "2.0.0-rc.2"
 
 config = mw.addonManager.getConfig(__name__)
 #print("config is", config)
 
 # check anki version. v2.1.17 has new API
 anki_api_since_2_1_17 = tuple(int(i) for i in version.split(".")) >= (2,1,17)
+#print("version is", version)
 
 if anki_api_since_2_1_17:
     from aqt.browser import SidebarItem
+
+icon_deck = ":/icons/deck.svg"
+icon_tag = ":/icons/tag.svg"
+icon_notetype = ":/icons/notetype.svg"
+icon_heart = ":/icons/heart.svg"
 
 #########################################
 # show some items
@@ -26,23 +33,23 @@ if anki_api_since_2_1_17:
 def custom_stdTree_until_2_1_17(self, root):
     if config['show_item_marked']:
         item = self.CallbackItem(root, _("Marked"), self._filterFunc("tag:marked"))
-        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item.setIcon(0, QIcon(icon_tag))
     if config['show_item_suspended']:
         item = self.CallbackItem(root, _("Suspended"), self._filterFunc("is:suspended"))
-        item.setIcon(0, QIcon(":/icons/heart.svg"))
+        item.setIcon(0, QIcon(icon_tag))
     if config['show_item_leech']:
         item = self.CallbackItem(root, _("Leech"), self._filterFunc("tag:leech"))
-        item.setIcon(0, QIcon(":/icons/tag.svg"))
+        item.setIcon(0, QIcon(icon_tag))
     if config['show_tree_today']:
         today = self.CallbackItem(root, _("Today"), None)
-        today.setIcon(0, QIcon(":/icons/tag.svg"))
+        today.setIcon(0, QIcon(icon_tag))
         today.setExpanded(False)
         item = self.CallbackItem(today, _("Added Today"), self._filterFunc("added:1"))
         item = self.CallbackItem(today, _("Studied Today"), self._filterFunc("rated:1"))
         item = self.CallbackItem(today, _("Again Today"), self._filterFunc("rated:1:1"))
     if config['show_tree_flags']:
         flags = self.CallbackItem(root, _("Flags"), None)
-        flags.setIcon(0, QIcon(":/icons/tag.svg"))
+        flags.setIcon(0, QIcon(icon_tag))
         flags.setExpanded(False)
         item = self.CallbackItem(flags, _("Red"), self._filterFunc("flag:1"))
         item = self.CallbackItem(flags, _("Orange"), self._filterFunc("flag:2"))
@@ -53,16 +60,16 @@ def custom_stdTree_until_2_1_17(self, root):
 
 def custom_stdTree_since_2_1_17(self, root):
     if config['show_item_marked']:
-        item = SidebarItem(_("Marked"), ":/icons/tag.svg", self._filterFunc("tag:marked"))
+        item = SidebarItem(_("Marked"), icon_tag, self._filterFunc("tag:marked"))
         root.addChild(item)
     if config['show_item_suspended']:
-        item = SidebarItem(_("Suspended"), ":/icons/tag.svg", self._filterFunc("is:suspended"))
+        item = SidebarItem(_("Suspended"), icon_tag, self._filterFunc("is:suspended"))
         root.addChild(item)
     if config['show_item_leech']:
-        item = SidebarItem(_("Leech"), ":/icons/tag.svg", self._filterFunc("tag:leech"))
+        item = SidebarItem(_("Leech"), icon_tag, self._filterFunc("tag:leech"))
         root.addChild(item)
     if config['show_tree_today']:
-        today = SidebarItem(_("Today"), ":/icons/tag.svg")
+        today = SidebarItem(_("Today"), icon_tag)
         item = SidebarItem(_("Added Today"), "", self._filterFunc("added:1"))
         today.addChild(item)
         item = SidebarItem(_("Studied Today"), "", self._filterFunc("rated:1"))
@@ -71,7 +78,7 @@ def custom_stdTree_since_2_1_17(self, root):
         today.addChild(item)
         root.addChild(today)
     if config['show_tree_flags']:
-        flags = SidebarItem(_("Flags"), ":/icons/tag.svg")
+        flags = SidebarItem(_("Flags"), icon_tag)
         item = SidebarItem(_("Red"), "", self._filterFunc("flag:1"))
         flags.addChild(item)
         item = SidebarItem(_("Orange"), "", self._filterFunc("flag:2"))
@@ -102,18 +109,18 @@ if config['show_item_marked'] or \
 def custom_modelTree_until_2_1_17(self, root, _old):
     root = self.CallbackItem(root, _("Note Types"), None)
     root.setExpanded(False)
-    root.setIcon(0, QIcon(":/icons/notetype.svg"))
+    root.setIcon(0, QIcon(icon_notetype))
     for m in sorted(self.col.models.all(), key=itemgetter("name")):
         mitem = self.CallbackItem(
             root, m['name'], lambda m=m: self.setFilter("note", str(m['name'])))
-        mitem.setIcon(0, QIcon(":/icons/notetype.svg"))
+        mitem.setIcon(0, QIcon(icon_notetype))
 
 def custom_modelTree_since_2_1_17(self, root, _old):
-    types = SidebarItem(_("Note Types"), ":/icons/notetype.svg")
+    types = SidebarItem(_("Note Types"), icon_notetype)
     for m in sorted(self.col.models.all(), key=itemgetter("name")):
         item = SidebarItem(
             m["name"],
-            ":/icons/notetype.svg",
+            icon_notetype,
             lambda m=m: self.setFilter("note", m["name"]),  # type: ignore
         )
         types.addChild(item)
@@ -133,17 +140,16 @@ if config['collapse_note_types']:
 def custom_favTree_until_2_1_17(self, root, _old):
     root = self.CallbackItem(root, _("My Filters"), None)
     root.setExpanded(False)
-    root.setIcon(0, QIcon(":/icons/heart.svg"))
+    root.setIcon(0, QIcon(icon_heart))
     saved = self.col.conf.get('savedFilters', {})
     for name, filt in sorted(saved.items()):
         item = self.CallbackItem(root, name, lambda s=filt: self.setFilter(s))
-        item.setIcon(0, QIcon(":/icons/heart.svg"))
+        item.setIcon(0, QIcon(icon_heart))
 
 def custom_favTree_since_2_1_17(self, root, _old):
-    favs = SidebarItem(_("My Filters"), ":/icons/heart.svg")
+    favs = SidebarItem(_("My Filters"), icon_heart)
     for name, filt in sorted(self.col.conf.get('savedFilters', {}).items()):
-        item = SidebarItem(name, ":/icons/heart.svg",
-                           lambda s=filt: self.setFilter(s))
+        item = SidebarItem(name, icon_heart, lambda s=filt: self.setFilter(s))
         favs.addChild(item)
     root.addChild(favs)
 
@@ -163,7 +169,7 @@ def custom_decksTree_until_2_1_17(self, root, _old):
         #print("item text is '%s'" % (item.text(0)))
     root = self.CallbackItem(root, _("Decks"), None)
     root.setExpanded(False)
-    root.setIcon(0, QIcon(":/icons/deck.svg"))
+    root.setIcon(0, QIcon(icon_deck))
     grps = self.col.sched.deckDueTree()
     def fillGroups(root, grps, head=""):
         for g in grps:
@@ -172,19 +178,19 @@ def custom_decksTree_until_2_1_17(self, root, _old):
                 lambda g=g: self.setFilter("deck", head+g[0]),
                 lambda g=g: self.mw.col.decks.collapseBrowser(g[1]),
                 not self.mw.col.decks.get(g[1]).get('browserCollapsed', False))
-            item.setIcon(0, QIcon(":/icons/deck.svg"))
+            item.setIcon(0, QIcon(icon_deck))
             newhead = head + g[0]+"::"
             fillGroups(item, g[5], newhead)
     fillGroups(root, grps)
 
 def custom_decksTree_since_2_1_17(self, root, _old):
-    decks = SidebarItem(_("Decks"), ":/icons/deck.svg")
+    decks = SidebarItem(_("Decks"), icon_deck)
     grps = self.col.sched.deckDueTree()
     def fillGroups(root, grps, head=""):
         for g in grps:
             item = SidebarItem(
                 g[0],
-                ":/icons/deck.svg",
+                icon_deck,
                 lambda g=g: self.setFilter("deck", head + g[0]),
                 lambda expanded, g=g: self.mw.col.decks.collapseBrowser(g[1]),
                 not self.mw.col.decks.get(g[1]).get("browserCollapsed", False),
